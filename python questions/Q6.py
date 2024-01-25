78bfa6617f940733aa271fb676a2a81f8e2a8089
tanmaybhardwaj2004 <116267785+tanmaybhardwaj2004@users.noreply.github.com>
2024-01-25 21:34:34 +0530

    Delete python questions directory

--- python questions/Q6.py
+++
@@ -1,9 +0,0 @@
-lst= []
-num=int(input('No of elements: '))
-for n in range(num):
-    numbers= int(input('Enter numbers') )
-    lst.append(numbers)
-    set_res= set(lst)
-    list_res= (list(set_res))
-    for item in list_res:
-        print(item)
